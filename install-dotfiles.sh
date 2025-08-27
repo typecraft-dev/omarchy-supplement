47536e4191091f3108c930554c4e42ee3a09bedf
chris power <chris@typecraft.dev>
2025-08-27 14:08:34 -0400

    update overrides for new omarchy version

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -24,11 +24,17 @@ fi
 
 # Check if the clone was successful
 if [ $? -eq 0 ]; then
+  echo "removing old configs"
+  rm -rf ~/.config/nvim ~/.config/starship.toml ~/.local/share/nvim/ ~/.cache/nvim/ ~/.config/ghostty/config
+
   cd "$REPO_NAME"
   stow zshrc
+  stow ghostty
+  stow tmux
   stow nvim
   stow starship
 else
   echo "Failed to clone the repository."
   exit 1
 fi
+
